954489f6f6e2f03898eb7684f908370481a1aee6
Ensieh Hassanzadeh <ensieh.hassanzadeh@gmail.com>
2024-02-02 18:27:16 +0330

    feat: add api create contract

--- src/datasource/API/ContractAPI.ts
+++ src/datasource/API/ContractAPI.ts
@@ -13,3 +13,34 @@ export interface API_Contract_List_Output {
 export function API_Contract_List() {
   return httpRequest<API_Contract_List_Output>('/contracts/list/')
 }
+
+export interface API_Contract_Create_Input {
+  title?: string
+  clauses: {
+    title?: string
+    content: string
+    remarks: {
+      title?: string
+      content: string
+    }[]
+  }[]
+}
+
+export interface API_Contract_Create_Output {
+  id: number
+  title: string
+  clauses: {
+    id: number
+    title: string
+    content: string
+    remarks: {
+      id: number
+      title: string
+      content: string
+    }[]
+  }[]
+}
+
+export function API_Contract_Create(body: API_Contract_Create_Input) {
+  return httpRequest<API_Contract_Create_Output>('/contracts/contract/create/', { body, method: 'POST' })
+}
--- src/stores/contract.ts
+++ src/stores/contract.ts
@@ -1,4 +1,4 @@
-import { API_Contract_List } from "@/datasource/API/ContractAPI"
+import { API_Contract_Create, API_Contract_Create_Input, API_Contract_List } from "@/datasource/API/ContractAPI"
 import { defineStore } from "pinia"
 
 export const useContractStore = defineStore('contract', () => {
@@ -6,6 +6,10 @@ export const useContractStore = defineStore('contract', () => {
   function list() {
     return API_Contract_List()
   }
+
+  function create(body: API_Contract_Create_Input) {
+    return API_Contract_Create(body)
+  }
   
-  return{ list }
+  return{ list, create }
 })
