20001026b5d41179130dc4721aa0272622908d54
Ensieh Hassanzadeh <ensieh.hassanzadeh@gmail.com>
2024-02-02 19:01:57 +0330

    feat: add api delete contract

--- src/datasource/API/ContractAPI.ts
+++ src/datasource/API/ContractAPI.ts
@@ -44,3 +44,21 @@ export interface API_Contract_Create_Output {
 export function API_Contract_Create(body: API_Contract_Create_Input) {
   return httpRequest<API_Contract_Create_Output>('/contracts/contract/create/', { body, method: 'POST' })
 }
+
+export interface API_Contract_Delete_Input {
+  /** کد احراز هویت */
+  ver_id: number
+  /** کد تاییدیه */
+  ver_code: string
+  /** شناسه قرارداد */
+  contract_id: number
+}
+
+export interface API_Contract_Delete_Output {
+  /** پیام */
+  message: string
+}
+
+export function API_Contract_Delete(body: API_Contract_Delete_Input) {
+  return httpRequest<API_Contract_Delete_Output>('/contracts/contract/delete/', { body, method: 'POST' })
+}
--- src/stores/contract.ts
+++ src/stores/contract.ts
@@ -1,4 +1,4 @@
-import { API_Contract_Create, API_Contract_Create_Input, API_Contract_List } from "@/datasource/API/ContractAPI"
+import { API_Contract_Create, API_Contract_Create_Input, API_Contract_List, API_Contract_Delete_Input, API_Contract_Delete } from "@/datasource/API/ContractAPI"
 import { defineStore } from "pinia"
 
 export const useContractStore = defineStore('contract', () => {
@@ -10,6 +10,10 @@ export const useContractStore = defineStore('contract', () => {
   function create(body: API_Contract_Create_Input) {
     return API_Contract_Create(body)
   }
-  
-  return{ list, create }
+
+  function deleteCntract (body: API_Contract_Delete_Input) {
+    return API_Contract_Delete(body)
+  }
+
+return { list, create, deleteCntract }
 })
